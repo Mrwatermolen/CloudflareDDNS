ec304fe21bec3b61c435f7988c4da84256475681
Franzero <onepagesword@gmail.com>
2026-03-06 23:46:37 +0800

    Test for asio 010

--- src/include/common.h
+++ src/include/common.h
@@ -13,22 +13,6 @@ struct Error {
   std::string message;
 };
 
-#define CHECK_HTTP_RESULT(res, msg)                                            \
-  do {                                                                         \
-    if (!(res)) {                                                              \
-      auto __err_msg = std::format("{}: network error: {}", msg,               \
-                                   httplib::to_string((res).error()));         \
-      LOG_ERROR(__err_msg);                                                    \
-      return std::unexpected<Error>{Error{.message = __err_msg}};              \
-    }                                                                          \
-    if ((res)->status != 200) {                                                \
-      LOG_ERROR(std::format("{}: http status: {}", msg, (res)->status));       \
-      auto __err_msg = std::format("{}: request failed, status: {}, body: {}", \
-                                   msg, (res)->status, (res)->body);           \
-      return std::unexpected<Error>{Error{.message = __err_msg}};              \
-    }                                                                          \
-  } while (0)
-
 class IpResolver {
  public:
   IpResolver() = default;
@@ -43,6 +27,14 @@ class IpResolver {
 };
 
 template <typename T>
+concept IpResolverConcept = requires(T t) {
+  { t.resolve() } -> std::same_as<std::expected<std::string, Error>>;
+  {
+    t.resolveAsync(std::function<void(std::expected<std::string, Error>)>())
+  } -> std::same_as<void>;
+};
+
+template <IpResolverConcept T>
 class IpResolverWrapper : public IpResolver {
  public:
   template <typename... Args>
